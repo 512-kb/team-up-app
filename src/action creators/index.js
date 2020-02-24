b08e594f98c047dd3989ed4cb74cf4216d468abd
512-kb <kb98kb98kb@gmail.com>
2020-02-24 15:02:53 +0530

    load user data

--- src/action creators/index.js
+++ src/action creators/index.js
@@ -11,3 +11,19 @@ export const registerUser = formValues => async dispatch => {
   const user = await axios.post("/register", formValues);
   dispatch({ type: "REGISTER_USER", payload: user.data });
 };
+
+export const loadUserData = username => async dispatch => {
+  const channels = (await axios.get("/channels", { params: { username } }))
+    .data;
+  const invites = (await axios.get("/invitations", { params: { username } }))
+    .data;
+  dispatch({
+    type: "LOAD_USER_DATA",
+    payload: { channels, invites }
+  });
+};
+
+export const loadTop5 = () => async dispatch => {
+  const top5 = (await axios.get("/top5")).data;
+  dispatch({ type: "FETCH_TOP5", payload: { top5 } });
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,37 @@
 import { combineReducers } from "redux";
+import _ from "lodash";
 
-const loginReducer = (user = { _id: false }, action) => {
+const loginReducer = (user = { username: false }, action) => {
   if (action.type === "LOGIN_USER") {
     return action.payload;
   }
   return user;
 };
 
-const registerReducer = (user = { _id: false }, action) => {
+const registerReducer = (user = { username: false }, action) => {
   if (action.type === "REGISTER_USER") {
     return action.payload;
   }
   return user;
 };
 
+const userDataReducer = (userData = {}, action) => {
+  if (action.type === "LOAD_USER_DATA") {
+    return _.assign(userData, action.payload);
+  }
+  return userData;
+};
+
+const top5Reducer = (top5 = [], action) => {
+  if (action.type === "FETCH_TOP5") {
+    return action.payload;
+  }
+  return top5;
+};
+
 export default combineReducers({
   user: loginReducer,
-  registeredUser: registerReducer
+  registeredUser: registerReducer,
+  userData: userDataReducer,
+  top5: top5Reducer
 });
