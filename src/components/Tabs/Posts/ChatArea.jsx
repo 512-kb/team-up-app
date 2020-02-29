3114d97af78599c7224a6d6cbea60790c146d86a
512-kb <kb98kb98kb@gmail.com>
2020-02-29 17:58:02 +0530

    create post  ui

--- src/components/Tabs/Posts/ChatArea.jsx
+++ src/components/Tabs/Posts/ChatArea.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
-import { Segment, Dropdown } from "semantic-ui-react";
+import { TextArea, Segment, Dropdown, Button } from "semantic-ui-react";
 import { connect } from "react-redux";
 
 class ChatArea extends Component {
-  state = {};
+  state = { content: "", tags: [], error: false };
+
+  handleChange = (e, { name, value }) =>
+    this.setState({
+      [name]: name === "content" ? value.replace(/\s\s+/g, " ") : value
+    });
+
+  validate = () => {
+    if (this.state.content.length < 1) {
+      this.setState({ error: "Invalid Content" });
+      return false;
+    }
+    this.setState({ error: false });
+    return true;
+  };
+
   render() {
     return this.props.activeChannel ? (
       <React.Fragment>
@@ -11,27 +26,46 @@ class ChatArea extends Component {
           style={{
             position: "relative",
             overflowY: "auto",
-            height: "80%",
+            height: "70%",
             marginBottom: "0"
           }}
         ></Segment>
         <Segment
           style={{
             position: "relative",
-            height: "20%",
+            height: "30%",
             marginTop: "0",
-            marginBottom: "0"
+            top: "0",
+            bottom: "0"
           }}
         >
+          <TextArea
+            name="content"
+            placeholder="Post Content"
+            style={{
+              position: "relative",
+              width: "100%",
+              height: "50%",
+              resize: "none"
+            }}
+            onChange={this.handleChange}
+          />
+          <br />
           <Dropdown
+            name="tags"
             placeholder="Tags"
+            style={{ position: "relative", marginBottom: "0.3%" }}
             multiple
             selection
-            onChange={(e, item) => console.log(item)}
             options={this.props.activeChannel.tags.map(tag => {
               return { key: tag, text: tag, value: tag };
             })}
+            onChange={this.handleChange}
           />
+          <br />
+          <Button color="green" floated="right" onClick={this.validate}>
+            POST
+          </Button>
         </Segment>
       </React.Fragment>
     ) : (
