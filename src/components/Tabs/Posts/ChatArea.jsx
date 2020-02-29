63f82857db7c9361dc2dd8fad92eb2ae4fc7f559
512-kb <kb98kb98kb@gmail.com>
2020-02-29 18:59:50 +0530

    post creator

--- src/components/Tabs/Posts/ChatArea.jsx
+++ src/components/Tabs/Posts/ChatArea.jsx
@@ -1,23 +1,10 @@
 import React, { Component } from "react";
-import { TextArea, Segment, Dropdown, Button } from "semantic-ui-react";
+import { Segment } from "semantic-ui-react";
 import { connect } from "react-redux";
+import PostCreator from "./PostCreator";
 
 class ChatArea extends Component {
-  state = { content: "", tags: [], error: false };
-
-  handleChange = (e, { name, value }) =>
-    this.setState({
-      [name]: name === "content" ? value.replace(/\s\s+/g, " ") : value
-    });
-
-  validate = () => {
-    if (this.state.content.length < 1) {
-      this.setState({ error: "Invalid Content" });
-      return false;
-    }
-    this.setState({ error: false });
-    return true;
-  };
+  state = {};
 
   render() {
     return this.props.activeChannel ? (
@@ -30,43 +17,7 @@ class ChatArea extends Component {
             marginBottom: "0"
           }}
         ></Segment>
-        <Segment
-          style={{
-            position: "relative",
-            height: "30%",
-            marginTop: "0",
-            top: "0",
-            bottom: "0"
-          }}
-        >
-          <TextArea
-            name="content"
-            placeholder="Post Content"
-            style={{
-              position: "relative",
-              width: "100%",
-              height: "50%",
-              resize: "none"
-            }}
-            onChange={this.handleChange}
-          />
-          <br />
-          <Dropdown
-            name="tags"
-            placeholder="Tags"
-            style={{ position: "relative", marginBottom: "0.3%" }}
-            multiple
-            selection
-            options={this.props.activeChannel.tags.map(tag => {
-              return { key: tag, text: tag, value: tag };
-            })}
-            onChange={this.handleChange}
-          />
-          <br />
-          <Button color="green" floated="right" onClick={this.validate}>
-            POST
-          </Button>
-        </Segment>
+        <PostCreator />
       </React.Fragment>
     ) : (
       <React.Fragment>Loading..</React.Fragment>
@@ -74,8 +25,4 @@ class ChatArea extends Component {
   }
 }
 
-const getActiveChannel = ({ activeChannel, user }) => {
-  return { activeChannel, user };
-};
-
-export default connect(getActiveChannel)(ChatArea);
+export default connect(null)(ChatArea);
