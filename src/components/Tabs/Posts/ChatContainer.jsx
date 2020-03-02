850c42d884cc7a2d353186bc723565c4f1226491
512-kb <kb98kb98kb@gmail.com>
2020-03-02 22:38:20 +0530

    old post fetch & autoscrolling added

--- src/components/Tabs/Posts/ChatContainer.jsx
+++ src/components/Tabs/Posts/ChatContainer.jsx
@@ -2,35 +2,51 @@ import React, { Component } from "react";
 import { Segment, Feed, Label } from "semantic-ui-react";
 import moment from "moment";
 import { connect } from "react-redux";
-import { updatePosts } from "../../../action creators";
+import { updatePosts, loadPosts } from "../../../action creators";
 import socket from "../../../sockets";
 
 class ChatContainer extends Component {
-  state = { activeChannel: false };
+  state = { activeChannel: false, page: 0 };
 
   componentDidMount = () => {
     socket.on("new_post_braodcast", obj => {
-      console.log(obj);
       this.props.updatePosts(obj);
+      this.scrollToBottom();
     });
-    this.scrollToBottom();
+
+    let container = document.getElementById("chatContainer");
+    container.onscroll = () => {
+      if (container.scrollTop === 0) {
+        socket.emit(
+          "fetch_old_posts",
+          { channel_id: this.props.activeChannel._id, page: this.state.page },
+          this.props.loadPosts
+        );
+        this.incrementPage();
+      }
+    };
   };
 
-  static getDerivedStateFromProps(props, state) {
+  incrementPage = () => this.setState({ page: this.state.page + 1 });
+
+  static getDerivedStateFromProps = (props, state) => {
     //console.log(props);
     if (
       props.activeChannel &&
       props.activeChannel._id !== state.activeChannel._id
     ) {
-      socket.emit("join_channel_room", props.activeChannel._id);
-      return { activeChannel: props.activeChannel };
+      socket.emit(
+        "join_channel_room",
+        props.activeChannel._id,
+        props.loadPosts
+      );
+      return { activeChannel: props.activeChannel, page: 1 };
     }
     return state;
-  }
-  componentDidUpdate = () => this.scrollToBottom();
+  };
   scrollToBottom = () => {
-    let objDiv = document.getElementById("chatContainer");
-    objDiv.scrollTop = objDiv.scrollHeight;
+    let container = document.getElementById("chatContainer");
+    container.scrollTop = container.scrollHeight;
   };
   render() {
     return (
@@ -75,4 +91,6 @@ const getActiveChannel = ({ activeChannel, posts, user }) => {
   return { activeChannel, posts, user };
 };
 
-export default connect(getActiveChannel, { updatePosts })(ChatContainer);
+export default connect(getActiveChannel, { updatePosts, loadPosts })(
+  ChatContainer
+);
