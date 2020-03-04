9d78164921071014ee407eb6e7d426badd9b2cc4
512-kb <kb98kb98kb@gmail.com>
2020-03-04 21:06:12 +0530

    scroll fixed

---
+++ build/precache-manifest.b6e7cfdf48132bf81d8387ed774440c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bd7024ddfeb98ad54d10dfb69ae7b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9286c344f6a7b7fb3011",
+    "url": "/static/js/2.bf3e415b.chunk.js"
+  },
+  {
+    "revision": "38a590322cfdebf4cce3133cfa561d72",
+    "url": "/static/js/2.bf3e415b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c597d5b60f8fc8acc0",
+    "url": "/static/js/main.f2dc9f51.chunk.js"
+  },
+  {
+    "revision": "a0478af65bd914356dbc",
+    "url": "/static/js/runtime-main.4032331f.js"
+  }
+]);
